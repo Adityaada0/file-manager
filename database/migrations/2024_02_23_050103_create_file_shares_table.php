7fef1ad422678e80f2504e50a66cc5b096824740
Adityaada0 <adityastd2003@gmail.com>
2024-02-23 16:45:09 +0530

    initial commit

--- database/migrations/2024_02_23_050103_create_file_shares_table.php
+++ database/migrations/2024_02_23_050103_create_file_shares_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('file_shares', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('file_id')->nullable()->constrained('files');
+            $table->foreignId('user_id')->nullable()->constrained('users');
             $table->timestamps();
         });
     }
